cad62e07c487c1f77d201f6c58c902a7c22da5f7
hucong <453959527@qq.com>
2018-05-31 14:29:55 +0800

    add demo/spa-old/

---
+++ demo/spa-old/views/home/home.js
@@ -0,0 +1,12 @@
+(function(){
+
+var h1 = document.getElementsByTagName('h1')[0];
+if(h1){
+	h1.onclick = function(){
+		alert('home');
+	}	
+}
+
+
+})();
+
---
+++ demo/spa-old/views/user/user_detail.js
@@ -0,0 +1,2 @@
+var content = document.getElementById('id-test');
+content.textContent = hspa.id;
